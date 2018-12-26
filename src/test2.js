e090e7fd167b2c8d4444bb7746fef9dd37c7e25a
lesejong <umlstudy@gmail.com>
2018-12-26 15:52:02 +0900

    누구든지 하는 리액트 7편: 배열 다루기 (2) #1 배열복사

---
+++ src/test2.js
@@ -0,0 +1,14 @@
+// tslint:disable:no-console
+const arr = [1, 2, 3, 4, 5];
+
+// concat
+const arr2 = arr.slice(0,2).concat(arr.slice(3,5)) // [1, 2, 4, 5]
+console.log(arr2);
+
+// 전개연산자
+const arr3 = [ ...arr.slice(0,2), ...arr.slice(3,5) ];
+console.log(arr3);
+
+// 필터
+const arr4= arr.filter(num => num !== 3); // [1, 2, 4, 5]
+console.log(arr4);
