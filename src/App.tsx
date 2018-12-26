5d73ef4d3d0598d01cfa14212f8524263b05026b
lesejong <umlstudy@gmail.com>
2018-12-26 15:09:12 +0900

    누구든지 하는 리액트 7편: 배열 다루기 (1) 생성과 렌더링 #1

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,45 @@ import PhoneForm from './component/PhoneForm';
 // tslint:disable:no-console
 class App extends React.Component {
 
+  public state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
+  private id: number = 2
+
   public render() {
+    const { information } = this.state;
     return (
-      <div className="App">
+      <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
+        {information.forEach((item)=>{
+          //
+          JSON.stringify(item)
+          ;
+        })}
+        {JSON.stringify(information)}
       </div>
     );
   }
 
-  private handleCreate = (data: any) => {
-    console.log('App.handleCreate called');
-    console.log(data);
-  };
+  public handleCreate = (data: any) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
 }
 
 export default App;
