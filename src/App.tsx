db32d5d9ef0628478297e8655c810b43c2288d28
lesejong <umlstudy@gmail.com>
2018-12-26 14:55:18 +0900

    누구든지 하는 리액트 6편: input 상태 관리하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import * as React from 'react';
 import './App.css';
+import PhoneForm from './component/PhoneForm';
 
-import Counter from './Counter';
-import LifeCycle from './LifeCycle';
-import logo from './logo.svg';
-
+// tslint:disable:no-console
 class App extends React.Component {
+
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Counter />
-        <LifeCycle />
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
       </div>
     );
   }
+
+  private handleCreate = (data: any) => {
+    console.log('App.handleCreate called');
+    console.log(data);
+  };
 }
 
 export default App;
