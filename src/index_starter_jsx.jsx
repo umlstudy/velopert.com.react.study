613faafd6e7b8d696bc3a0b1495a28c4f30f51b5
lesejong <umlstudy@gmail.com>
2019-01-10 09:32:34 +0900

    미들웨어(Middleware) 이해하기

--- src/index_starter_jsx.jsx
+++ src/index_starter_jsx.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware  } from 'redux';
 import { Provider  } from 'react-redux';
 import App from './reducer/components/App';
 import counterApp from './reducer/reducers';
+import loggerMiddleware from './middleware/lib/loggerMiddleware';
 
+// tslint:disable:no-console
 export default function main() {
 
-const store = createStore(counterApp);
-const appElement = document.getElementById('root');
+    const appElement = document.getElementById('root');
+    
+    
+    // 미들웨어가 여러개인경우에는 파라미터로 여러개를 전달해주면 됩니다. 예: applyMiddleware(a,b,c)
+    // 미들웨어의 순서는 여기서 전달한 파라미터의 순서대로 지정됩니다.
+    //const store = createStore(counterApp);
+    const store = createStore(counterApp, applyMiddleware(loggerMiddleware))
 
 
-// 렌더링 될 때 Redux 컴포넌트인 <Provider> 에 store 를 설정해주면 
-// 그 하위 컴포넌트들에 따로 parent-child 구조로 전달해주지 않아도 
-// connect 될 때 store에 접근 할 수 있게 해줍니다.
-ReactDOM.render(
-    <Provider store = {store}>
-        <App />
-    </Provider>,
-    appElement
-);
+    // 렌더링 될 때 Redux 컴포넌트인 <Provider> 에 store 를 설정해주면 
+    // 그 하위 컴포넌트들에 따로 parent-child 구조로 전달해주지 않아도 
+    // connect 될 때 store에 접근 할 수 있게 해줍니다.
+    ReactDOM.render(
+        <Provider store = {store}>
+            <App />
+        </Provider>,
+        appElement
+    );
+
+    // curry test
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAABB");
+
+    let func = a => b => c => {
+        return a + b + c;
+    };
+
+    let funca = func(3);
+    let funcb = funca(7);
+    let funcc = funcb(11);
+
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAABB => " + funcc);
 
 }
 
